c6d8f6dc5db86113681795b90554bc9ad004f3a9
DooBae-KR <DOOBAE-KR>
2025-07-03 17:58:14 +0900

    STYLE: sidebar 페이징, css 완

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,45 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import './styles/login.css';
 import './styles/regist.css';
+import './styles/components/tabMenu.css';
+import './styles/components/sideBar.css';
 
-//Page imports
+//Page import
 import MapPage from './pages/MapPage.js';
 import LoginPage from './pages/login/LogInPage.js';
 import RegistUserPage from './pages/login/RegistUserPage.js';
+import TabMenu from './components/TabMenu.js'
+import BarBest from './pages/sideBar/TabBestRes.js';
+import BarClean from './pages/sideBar/TabCleanGrd.js';
+import BarMy from './pages/sideBar/TabMyPage.js';
+import BarPenel from './pages/sideBar/TabPenel.js';
+import BarSearch from './pages/sideBar/TabSearch.js';
 
 
 function App() {
   return (
-
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MapPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/regist" element={<RegistUserPage />} />
-        </Routes>
+          <div style={{display:'flex',minHeight: '100vh'}}>
+            <div style={{ flex:0 }}>
+              <TabMenu />
+            </div>
+            
+            <div style={{flex:1}}>
+              <Routes>
+                {/* 일반 페이지  */}
+                <Route path="/" element={<MapPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/regist" element={<RegistUserPage />} />
+
+                {/* 사이드바 관련 라우트 */}
+                <Route path="/sidebar/search" element={<BarSearch />} />
+                <Route path="/sidebar/clean" element={<BarClean />} />
+                <Route path="/sidebar/mypage" element={<BarMy />} />
+                <Route path="/sidebar/penel" element={<BarPenel />} />
+                <Route path="/sidebar/best" element={<BarBest />} />
+              </Routes>
+            </div>
+          </div>
       </BrowserRouter>
 
   );
---
+++ frontend/src/pages/sideBar/TabSearch.js
@@ -0,0 +1,35 @@
+export default function TabSearch(){
+    return (
+        <div class="sideBar">
+            <div id="home_search_input_box" class="sc-1ooij2a hqeKHp">
+                <div class="search_wrap ">
+                    <div class="search_box">
+                        <button type="button" class="button_search">검색</button>
+                        <div class="input_box">
+                            <label for="input_search1751511758441" class="label_search">장소, 버스, 지하철, 도로 검색</label>
+                            <input type="text" id="input_search1751511758441" class="input_search" autocomplete="off" maxlength="255" role="combobox" aria-controls="home-search-input" aria-expanded="false" aria-activedescendant="" value=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sc-1sch9h3 gjDQwY">
+                <div class="panel_content" id="section_content">
+                    <div class="sc-119eeef jKupKC">
+                    <h2 class="blind">지도 홈</h2>
+                    <div class="sc-sqxx1p beplGE">
+                    <div class="notice_wrap">
+                    <h3 class="blind">공지사항</h3>
+                    <button type="button" class="btn_login">로그인</button>
+                    <button type="button" class="btn_close"><span class="blind">닫기</span></button>
+                    </div>
+                    </div>
+                    <div class="sc-s1iuik eeZlt">
+                        <div class="sc-1i9k1xg eXgxoR">
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
