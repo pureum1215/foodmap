d527dd169ffb85c7e7426ff32558e2c00edd343d
pureum1215 <pureum1215@naver.com>
2025-07-07 11:16:12 +0900

    controller, mapper 추가

--- backend/src/main/java/kr/map/food/mapper/apiData/BestRestaurantApiDataMapper.java
+++ backend/src/main/java/kr/map/food/mapper/apiData/BestRestaurantApiDataMapper.java
@@ -1,5 +1,18 @@
 package kr.map.food.mapper.apiData;
 
-public class BestRestaurantApiDataMapper {
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.map.food.domain.apiData.bestRestaurant.BestRestaurantDTO;
+
+@Mapper
+public interface BestRestaurantApiDataMapper {
+
+    void insertBestRestaurant(BestRestaurantDTO dto);
+
+    List<BestRestaurantDTO> selectAll();
+
+    BestRestaurantDTO selectById(String RESIDX);
     
 }
